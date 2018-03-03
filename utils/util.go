85fea87d507f1ebb33fec12d29d14bfdbdb1e08e
TruthHun <TruthHun@QQ.COM>
2018-03-04 07:54:38 +0800

    showImg模板函数优化

--- utils/util.go
+++ utils/util.go
@@ -230,16 +230,20 @@ func CrawlHtml2Markdown(urlstr string, contType int, force, intelligence bool, h
 
 //操作图片显示
 //如果用的是oss存储，这style是avatar、cover可选项
-func HandlePicShow(img string, style string) (url string) {
+func HandlePicShow(img string, style ...string) (url string) {
 	if strings.HasPrefix(img, "https://") || strings.HasPrefix(img, "http://") {
 		return img
 	}
 	img = "/" + strings.TrimLeft(img, "./")
 	switch StoreType {
 	case StoreOss:
-		url = strings.TrimRight(beego.AppConfig.String("oss::Domain"), "/ ") + img + "/" + style
+		s := ""
+		if len(style) > 0 && strings.TrimSpace(style[0]) != "" {
+			s = "/" + style[0]
+		}
+		url = strings.TrimRight(beego.AppConfig.String("oss::Domain"), "/ ") + img + s
 	case StoreLocal:
-		url = "/" + strings.TrimLeft(img, "./")
+		url = img
 	}
 	return
 }
