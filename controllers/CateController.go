7c5ff41d5a66595c22ad6c20024ff5d6aa012550
TruthHun <TruthHun@QQ.COM>
2018-03-28 07:28:45 +0800

    分类

--- controllers/CateController.go
+++ controllers/CateController.go
@@ -1,5 +1,10 @@
 package controllers
 
+import (
+	"github.com/TruthHun/BookStack/models"
+	"github.com/astaxie/beego"
+)
+
 type CateController struct {
 	BaseController
 }
@@ -7,5 +12,10 @@ type CateController struct {
 //分类
 func (this *CateController) List() {
 	this.Data["IsCate"] = true
+	if cates, err := new(models.Category).GetCates(-1, 1); err == nil {
+		this.Data["Cates"] = cates
+	} else {
+		beego.Error(err.Error())
+	}
 	this.TplName = "cates/list.html"
 }
