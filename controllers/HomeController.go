d5f07f634efc0239e850d9bff80ca141a39878b5
进击的皇虫 <truthhun@qq.com>
2020-03-29 11:15:12 +0800

    显示分类介绍

--- controllers/HomeController.go
+++ controllers/HomeController.go
@@ -93,6 +93,8 @@ func (this *HomeController) Index() {
 		title = "探索，发现新世界，畅想新知识 - " + this.Sitename
 	}
 
+	this.Data["Cate"] = cate
+
 	this.GetSeoByPage("index", map[string]string{
 		"title":       title,
 		"keywords":    "文档托管,在线创作,文档在线管理,在线知识管理,文档托管平台,在线写书,文档在线转换,在线编辑,在线阅读,开发手册,api手册,文档在线学习,技术文档,在线编辑",
