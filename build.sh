175f0ccbb51fe9a7a16ba820014145adc3da454b
truthhun <1272881215@qq.com>
2023-07-03 11:43:23 +0800

    构建脚本

--- build.sh
+++ build.sh
@@ -13,9 +13,29 @@ LDFLAGS="-s -w -X github.com/TruthHun/BookStack/utils.GitHash=${GITHASH} -X gith
 
 ##########
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -o output/mac/BookStack -ldflags "${LDFLAGS}"
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -o output/linux/BookStack -ldflags "${LDFLAGS}"
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -v -o output/windows/BookStack.exe -ldflags "${LDFLAGS}"
-upx -f -9 output/mac/BookStack
-upx -f -9 output/linux/BookStack
-upx -f -9 output/windows/BookStack.exe
+rm -rf output/${VERSION}
+mkdir -p output/${VERSION}
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -o output/${VERSION}/mac/BookStack -ldflags "${LDFLAGS}"
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -o output/${VERSION}/linux/BookStack -ldflags "${LDFLAGS}"
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -v -o output/${VERSION}/windows/BookStack.exe -ldflags "${LDFLAGS}"
+
+upx -f -9 output/${VERSION}/mac/BookStack
+upx -f -9 output/${VERSION}/linux/BookStack
+upx -f -9 output/${VERSION}/windows/BookStack.exe
+
+cp -r conf output/${VERSION}/mac/
+cp -r conf output/${VERSION}/linux/
+cp -r conf output/${VERSION}/windows/
+
+cp -r views output/${VERSION}/mac/
+cp -r views output/${VERSION}/linux/
+cp -r views output/${VERSION}/windows/
+
+cp -r dictionary output/${VERSION}/mac/
+cp -r dictionary output/${VERSION}/linux/
+cp -r dictionary output/${VERSION}/windows/
+
+cp -r static output/${VERSION}/mac/
+cp -r static output/${VERSION}/linux/
+cp -r static output/${VERSION}/windows/
